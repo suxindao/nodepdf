d0c3e7306968e12c01d69b6df89f2e0a7136eb64
DESKTOP-BEFC36O\suxin <suxindao@126.com>
2023-07-08 19:17:27 +0800

    添加字体

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const pdfService = require('../service/pdf-service');
 
 const router = express.Router();
-router.get('/invoice', (req, res, next) => {
-  const stream = res.writeHead(200, {
-    'Content-Type': 'application/pdf',
-    'Content-Disposition': `attachment;filename=invoice.pdf`,
-  });
-  pdfService.buildPDF(
-    (chunk) => stream.write(chunk),
-    () => stream.end()
-  );
+router.get('/', (req, res, next) => {
+    const stream = res.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': `attachment;filename=invoice.pdf`,
+    });
+    pdfService.buildPDF(
+        (chunk) => stream.write(chunk),
+        () => stream.end()
+    );
 });
 
 module.exports = router;
--- service/pdf-service.js
+++ service/pdf-service.js
@@ -1,19 +1,116 @@
 const PDFDocument = require('pdfkit');
 
-function buildPDF(dataCallback, endCallback) {
-  const doc = new PDFDocument({ bufferPages: true, font: 'Courier' });
-
-  doc.on('data', dataCallback);
-  doc.on('end', endCallback);
-
-  doc.fontSize(20).text(`A heading`);
-
-  doc
-    .fontSize(12)
-    .text(
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, saepe.`
-    );
-  doc.end();
+const px = em => em * 13
+const wordSpacing = _ => {
+    return {
+        "wordSpacing": px(_)
+    }
+}
+const stl_13 = _ => {
+    return {
+        "characterSpacing": px(-0.0408),
+        "indent": px(-0.0408)
+    }
 }
 
-module.exports = { buildPDF };
+const stl_17 = _ => px(0.800223)
+
+const stl_18 = _ => {
+    return {
+        "characterSpacing": 0,
+        "wordSpacing ": 0,
+    }
+}
+
+const stl_20 = _ => {
+    return {
+        "characterSpacing": px(-0.0306),
+        "wordSpacing ": px(-0.0306),
+    }
+}
+
+const stl_21 = _ => px(0.600167)
+
+const stl_22 = _ => {
+    return {
+        "characterSpacing": px(-0.0347),
+        "wordSpacing ": px(-0.0347),
+    }
+}
+
+const stl_28 = _ => {
+    return {
+        "characterSpacing": px(-0.0445),
+        "wordSpacing ": px(-0.0445),
+    }
+}
+
+function buildPDF(dataCallback, endCallback) {
+
+    const doc = new PDFDocument({
+        autoFirstPage: false,
+        bufferPages: true,
+        font: 'Courier',
+        size: 'A4'
+    });
+
+    doc.on('data', dataCallback);
+    doc.on('end', endCallback);
+
+    doc.addPage({
+        margins: {
+            top: 10,
+            bottom: 10,
+            left: 10,
+            right: 10
+        }
+    })
+
+    doc.font(`${__dirname}/simsun.ttf`)
+        .fontSize(px(1.10))
+        .fillColor('#FF0000')
+        .text(`账务明细清单`, px(21.3053), px(1.2696), Object.assign({}, stl_13()))
+
+    doc.font(`${__dirname}/times.ttf`)
+        .fontSize(px(0.9))
+        .fillColor('#000000')
+        .text(`Statement Of Account`, px(21.4053), px(2.5319), Object.assign({}, wordSpacing(0.0408), stl_13()))
+
+    doc.font(`${__dirname}/simsun.ttf`)
+        .fontSize(stl_17())
+        .fillColor('#000000')
+        .text(`开户银行:`, px(2.6251), px(3.4415), Object.assign({}, stl_18()))
+
+    doc.font(`${__dirname}/simsun.ttf`)
+        .fontSize(stl_17())
+        .fillColor('#000000')
+        .text(`南京分行郁金香路支行`, px(2.6251) + px(4), px(3.4415), Object.assign({}, stl_18()))
+
+    doc.font(`${__dirname}/simsun.ttf`)
+        .fontSize(stl_17())
+        .fillColor('#000000')
+        .text(`账单所属期间: `, px(34), px(3.4415), Object.assign({}, stl_18()))
+
+    doc.font(`${__dirname}/times.ttf`)
+        .fontSize(stl_17())
+        .fillColor('#000000')
+        .text(`20220501 20220531`, px(34) + px(5.1), px(3.4415), Object.assign({}, stl_20()))
+
+    doc.font(`${__dirname}/times.ttf`)
+        .fontSize(stl_21())
+        .fillColor('#000000')
+        .text(`A/COpeningBank`, px(2.6251), px(4.5037), Object.assign({}, stl_22()))
+
+    doc.font(`${__dirname}/times.ttf`)
+        .fontSize(stl_21())
+        .fillColor('#000000')
+        .text(`Statement CoveredPeriod`, px(34.484), px(4.5037), Object.assign({}, stl_28()))
+
+    doc.path('M 33,68 L 590,68 L 590,67.9 L 33,67.9')
+        .fillAndStroke('red', 'red')
+        .stroke()
+
+    doc.end();
+}
+
+module.exports = {buildPDF};
